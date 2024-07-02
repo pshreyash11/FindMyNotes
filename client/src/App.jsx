76dadab5b8cf120ff9c2c35ac6946c79cd98bbed
pshreyash11 <shreyashrpatil11@gmail.com>
2024-07-03 01:38:25 +0530

    react setup

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl text-red-600 font-bold underline">
+      Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
