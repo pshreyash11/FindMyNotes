dc14c7c8145120d9dbc9edb41bdfe7b02a454425
pshreyash11 <shreyashrpatil11@gmail.com>
2024-07-09 00:01:56 +0530

    header file isLoggedIn function

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
-import { Link , useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { MdOutlineFileUpload } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
@@ -8,67 +9,118 @@ import { GiHamburgerMenu } from "react-icons/gi";
 export const Header = () => {
   const navigate = useNavigate();
   axios.defaults.withCredentials = true;
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const result = await axios.get(
+          "http://localhost:5000/api/v1/users/get-user",
+        );
+        console.log("User received , response:", result.data);
+
+        if (result.data.status === "Error") {
+          alert("Please login first !");
+          setIsLoggedIn(false);
+          setUserInfo(null);
+          navigate("/");
+        } else {
+          console.log("User data : ", result);
+          setIsLoggedIn(true);
+          setUserInfo(result.data.data);
+          console.log("User info - ", userInfo);
+        }
+      } catch (error) {
+        setIsLoggedIn(false);
+        setUserInfo(null);
+        console.log(
+          "Some error happened while getting user data, Error: ",
+          error,
+        );
+      }
+    };
+
+    fetchUser();
+  }, [navigate]);
+
   const handleLogout = async () => {
     try {
-
-      const result = await axios.post("http://localhost:5000/api/v1/users/logout")
+      const result = await axios.post(
+        "http://localhost:5000/api/v1/users/logout",
+      );
       console.log("Server response:", result.data);
 
-      if(result.data.status ==="Error")
-      {
-        alert("Can't Logout user !")
+      if (result.data.statusCode >= 400) {
+        alert("Can't Logout user!");
+        navigate("/");
+      } else {
+        console.log("User Logged out Successfully: ", result);
+        setIsLoggedIn(false);
+        setUserInfo(null);
+        alert("User Logged out Successfully");
         navigate("/");
       }
-      else{
-        console.log("User Logged out Successfully: ", result);
-        alert("User Logged out Successfully");
-      navigate("/");
-      }
-
-
     } catch (error) {
-      alert("Can't Logout user .")
-      console.error("Some error happened while logging out user. Error: ",error.response ? error.response.data : error.message);
+      alert("Can't Logout user.");
+      console.error(
+        "Some error happened while logging out user. Error: ",
+        error.response ? error.response.data : error.message,
+      );
     }
-  }
-
+  };
 
   return (
     <header className="flex h-[80px] items-center justify-center border-b-2 border-black">
-      <div className="flex w-full max-w-[1550px] items-center justify-between ">
+      <div className="flex w-full max-w-[1550px] items-center justify-between">
         {/* Image Section */}
-        <div className="flex h-[60px] w-[120px] items-center justify-center overflow-hidden ">
+        <div className="flex h-[60px] w-[120px] items-center justify-center overflow-hidden">
           <img src="/logo.png" alt="logo" />
         </div>
-
-        <GiHamburgerMenu className="text-xl md:hidden"/>
-        <div className="hidden md:flex md:justify-center md:items-center md:gap-4">
+        {isLoggedIn ? (
+          <div className="flex justify-start items-start mx-0">
+            <h1 className="text-2xl font-bold">Hello {userInfo.username} !</h1>
+          </div>
+        ) : null}
+        <GiHamburgerMenu className="text-xl md:hidden" />
+        <div className="hidden md:flex md:items-center md:justify-center md:gap-4">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
-          {/* <Link to="/login">
-            <button className="rounded-xl bg-blue-500 px-5 py-2 font-bold  hover:bg-blue-700">
-              Login
-            </button>
-          </Link>
-          <Link to="/register">
-            <button className="rounded-xl bg-blue-500 px-5 py-2 font-bold  hover:bg-blue-700">
-              Register
-            </button>
-          </Link> */}
-          <Link to="/search" className="text-xl">
-            <FaSearch />
-          </Link>
-          <Link to="/upload" className="text-[24px]">
-            <MdOutlineFileUpload />
-          </Link>
-          <Link to="/profile">
-            <button className="rounded-xl bg-blue-500 px-5 py-2 font-bold hover:bg-blue-700">
-              Profile
-            </button>
-          </Link>
-          <button className="rounded-xl bg-blue-500 px-5 py-2 font-bold hover:bg-blue-700" onClick={handleLogout}>
-            Logout
-          </button>
+          {isLoggedIn ? (
+            <>
+              <Link to="/search" className="text-xl">
+                <FaSearch />
+              </Link>
+              <Link to="/upload" className="text-[24px]">
+                <MdOutlineFileUpload />
+              </Link>
+              <Link to="/profile">
+                <button className="rounded-xl bg-blue-500 px-5 py-2 font-bold hover:bg-blue-700">
+                  Profile
+                </button>
+              </Link>
+              <button
+                className="rounded-xl bg-blue-500 px-5 py-2 font-bold hover:bg-blue-700"
+                onClick={handleLogout}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to="/login">
+                <button className="rounded-xl bg-blue-500 px-5 py-2 font-bold hover:bg-blue-700">
+                  Login
+                </button>
+              </Link>
+              <Link to="/register">
+                <button className="rounded-xl bg-blue-500 px-5 py-2 font-bold hover:bg-blue-700">
+                  Register
+                </button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </header>
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser , refreshAccessToken} from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser , refreshAccessToken , getCurrentUser} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -20,6 +20,7 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT, logoutUser) 
 router.route("/refresh-token").post(refreshAccessToken)//this route is mostly hit by frontend dev.
+router.route("/get-user").get(verifyJWT,getCurrentUser)
 
 
 export default router;
