35b8776c193096f89875bbad1475d4a4dcbcd361
pshreyash11 <shreyashrpatil11@gmail.com>
2024-07-10 00:50:41 +0530

    profile page integrated with api

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,23 +1,44 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState , useContext} from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import axios from "axios";
+import UserContext from '../context/UserContext'
+
 
 const Profile = () => {
+
+  const {user} = useContext(UserContext)
+
+  const [userFiles, setUserFiles] = useState([]);
+
+  const userId = user._id;
+
+  useEffect(() => {
+    const getUserFiles = async () => {
+      const result = await axios.get(`http://localhost:5000/api/v1/notes/getFiles/${userId}`);
+      console.log(result.data);
+      setUserFiles(result.data.data);
+    };
+
+    getUserFiles();
+  }, [userId]);
+
+  const numberofUploads = userFiles.length;
 
   return (
     <div className="lg:h-heightWithoutNavbar flex flex-col items-center justify-center border border-red-500 lg:flex-row">
       <div className="flex w-full flex-col items-center justify-center border-[3px] border-green-500 py-4 lg:h-full lg:w-[40%]">
         <div className="grid h-[200px] w-[200px] place-content-center overflow-hidden rounded-full bg-gray-400 text-2xl font-black">
           {/* 200 x 200 */}
-          <img  alt="userprofile" className="" />
+          <img src={user.profileImage} alt="userprofile" className="" />
         </div>
         <div className="">
           <div className=" my-2 flex flex-col items-center justify-center ">
             <h2 className="text-2xl font-black">
-              <span>Shreyash</span> <span>Patil</span>
+              <span>{user.fullname}</span>
             </h2>
-            <p className="mt-1 text-center">pshreyash</p>
+            <p className="mt-1 text-center">{user.username}</p>
             <p className="mt-1 text-center">
-              This is first account
+              {user.bio}
             </p>
           </div>
         </div>
@@ -27,18 +48,25 @@ const Profile = () => {
             <p className="text-center text-[12px] font-bold">
               No. of Uploads :
             </p>
-            <p className="text-center text-5xl font-black">5</p>
+            <p className="text-center text-5xl font-black">{numberofUploads}</p>
           </div>
           <span className="h-[60px] w-[1px] bg-gray-400" />
-          <div className="grid h-[80px] w-[100px] place-content-center">
-            <p className="text-center text-[12px] font-bold">No. of Files :</p>
-            <p className="text-center text-5xl font-black">5</p>
-          </div>
         </div>
       </div>
       <div className="h-auto w-full border-[3px] border-amber-500 p-5 lg:h-full lg:w-[60%]">
         <h1 className="mb-3 text-xl font-black">My Documents :</h1>
-
+        <div className="grid grid-cols-1 gap-5 p-4 sm:grid-cols-2 md:grid-cols-3">
+          {userFiles.map((file) => (
+            <a
+              href={`http://localhost:6969/files/${file.files}`}
+              key={file._id}
+              className="mb-3 flex h-[35px] max-w-[250px] items-center justify-between gap-10 rounded-xl border border-black px-4"
+              target="_blank"
+            >
+              <p className="font-semibold"> {file.fileName}</p>
+            </a>
+          ))}
+        </div>
       </div>
     </div >
   );
