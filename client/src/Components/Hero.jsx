5b0ac3fe4a0eab55ed254f65b4031abf770afbf8
pshreyash11 <shreyashrpatil11@gmail.com>
2024-07-10 05:53:45 +0530

    some slight changes in hero.jsx

--- client/src/Components/Hero.jsx
+++ client/src/Components/Hero.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import UserContext from "../context/UserContext";
 
 export const Hero = () => {
+  const { isLoggedIn } = useContext(UserContext);
+
   return (
-    <div className="bg-unsplashBgImage relative flex h-full items-center justify-center bg-cover bg-center">
+    <div className="relative flex h-full items-center justify-center bg-unsplashBgImage bg-cover bg-center">
       <div className="absolute inset-0 bg-black opacity-70" />
       <div className="relative z-10 w-full max-w-[860px] text-center text-white">
         <h1 className="text-5xl font-bold md:text-5xl">FIND MY NOTES</h1>
@@ -14,23 +18,33 @@ export const Hero = () => {
           journey to academic excellence. Simplify your student life, make your
           notes work for you – discover a new era of innovation, start today
         </p>
-        {/* <div className="mt-5">
-          <Link to="/search">
-            <button className="rounded-xl text-blue-500 bg-white px-7 py-4 font-bold ">Get Started</button>
-          </Link>
-          <div className="flex gap-5 justify-center items-center">
-            <Link to="/login">
-              <button className="rounded-xl bg-white px-7 py-4 font-bold text-blue-500">
-                Login
-              </button>
-            </Link>
-            <Link to="/register">
-              <button className="rounded-xl bg-white px-7 py-4 font-bold text-blue-500">
-                Register
-              </button>
-            </Link>
-          </div>
-        </div> */}
+
+        <div className="mt-5">
+          {isLoggedIn ? (
+            <>
+              <Link to="/search">
+                <button className="rounded-xl bg-white px-7 py-4 font-bold text-blue-500">
+                  Get Started
+                </button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <div className="flex items-center justify-center gap-5">
+                <Link to="/login">
+                  <button className="rounded-xl bg-white px-7 py-4 font-bold text-blue-500">
+                    Login
+                  </button>
+                </Link>
+                <Link to="/register">
+                  <button className="rounded-xl bg-white px-7 py-4 font-bold text-blue-500">
+                    Register
+                  </button>
+                </Link>
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
